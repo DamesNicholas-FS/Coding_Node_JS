29ffaa072738af16548cc1732caf672257ea3de2
DamesNicholas-FS <nrdames@student.fullsail.edu>
2022-09-29 23:57:55 -0400

    upload example project file

--- app/app.js
+++ app/app.js
@@ -14,4 +14,17 @@ app.get("/", (req,res,next)=>{
 
 app.use('/example', router);
 
+app.use((req,res,next)=>{
+    const error = new Error("Not Found");
+    error.status = (404);
+    next(error)
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500).json({
+        message: error.message,
+        status: error.status,
+    })
+})
+
 module.exports = app;
--- router/router.js
+++ router/router.js
@@ -1,4 +1,15 @@
+const { request, response } = require("express");
 const express = require("express");
 const router = express.Router();
+
+router.get("/", (req, res)=>{
+    res.status(200).json({
+        message: "GET - SUCCESS",
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
 
 module.exports = router;
